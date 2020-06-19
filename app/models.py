f38cd427dd6dee37a90120f85b3cd25cde9ddc82
levovit <playersoft1999@gmail.com>
2020-06-19 23:55:00 +0300

    Created model and DB

--- app/models.py
+++ app/models.py
@@ -2,11 +2,19 @@ from app import db
 
 
 class User(db.Model):
-    __tablename__ = "User"
     telegram_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(40))
-    age = db.Column(db.Integer)
-    gender = db.Column(db.String(10))
+    name = db.Column(db.String(20), default="Anon")
+    age = db.Column(db.Integer, default=0)
+    gender = db.Column(db.String(20), default="")
+    state = db.Column(db.String(15), default="name")
 
     def __repr__(self):
         return f"{self.name} - {self.age}"
+
+    @classmethod
+    def get_state_by_id(cls, telegram_id):
+        user = cls.query.filter_by(telegram_id=telegram_id).first()
+        if user:
+            return user.state
+        else:
+            return False
--- config.py
+++ config.py
@@ -5,8 +5,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    TOKEN = os.environ.get('TOKEN') or ''
-    HOST_URL = os.environ.get('HOST_URL') or 'https://cd6e48a714f0.ngrok.io'
+    TOKEN = os.environ.get('TOKEN') or '1286782199:AAEgozwDkYV1s4Fmjd2A_eWLv5BZsHj-Ev8'
+    HOST_URL = os.environ.get('HOST_URL') or 'https://f7c65272f9c4.ngrok.io'
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
